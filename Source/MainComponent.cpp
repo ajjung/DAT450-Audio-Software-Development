a7021d3fede04424971947851cebf40d48633664
ajjung <ajjung20225@gmail.com>
2016-09-17 15:36:47 -0700

    MIDI Device
    Added MIDI Device

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,79 +16,123 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainContentComponent   : public AudioAppComponent
+class MainContentComponent : public Component,
+	private ComboBox::Listener,
+	private MidiInputCallback
 {
 public:
-    //==============================================================================
-    MainContentComponent()
-    {
-        setSize (800, 600);
+	MainContentComponent()
+		: lastInputIndex(0),
+		isAddingFromMidiInput(false)
+	{
+		addAndMakeVisible(midiInputListLabel);
+		midiInputListLabel.setText("MIDI Input:", dontSendNotification);
+		midiInputListLabel.attachToComponent(&midiInputList, true);
 
-        // specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
-    }
+		addAndMakeVisible(midiInputList);
+		midiInputList.setTextWhenNoChoicesAvailable("No MIDI Inputs Enabled");
+		const StringArray midiInputs(MidiInput::getDevices());
+		midiInputList.addItemList(midiInputs, 1);
+		midiInputList.addListener(this);
 
-    ~MainContentComponent()
-    {
-        shutdownAudio();
-    }
+		// find the first enabled device and use that by default
+		for (int i = 0; i < midiInputs.size(); ++i)
+		{
+			if (deviceManager.isMidiInputEnabled(midiInputs[i]))
+			{
+				setMidiInput(i);
+				break;
+			}
+		}
 
-    //==============================================================================
-    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
-    {
-        // This function will be called when the audio device is started, or when
-        // its settings (i.e. sample rate, block size, etc) are changed.
+		// if no enabled devices were found just use the first one in the list
+		if (midiInputList.getSelectedId() == 0)
+			setMidiInput(0);
 
-        // You can use this function to initialise any resources you might need,
-        // but be careful - it will be called on the audio thread, not the GUI thread.
+		setSize(600, 400);
+	}
 
-        // For more details, see the help for AudioProcessor::prepareToPlay()
-    }
+	~MainContentComponent()
+	{
+		deviceManager.removeMidiInputCallback(MidiInput::getDevices()[midiInputList.getSelectedItemIndex()], this);
+		midiInputList.removeListener(this);
+	}
 
-    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
-    {
-        // Your audio-processing code goes here!
+	void paint(Graphics& g) override
+	{
+		g.fillAll(Colours::grey);
+	}
 
-        // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-        // Right now we are not producing any data, in which case we need to clear the buffer
-        // (to prevent the output of random noise)
-        bufferToFill.clearActiveBufferRegion();
-    }
-
-    void releaseResources() override
-    {
-        // This will be called when the audio device stops, or when it is being
-        // restarted due to a setting change.
-
-        // For more details, see the help for AudioProcessor::releaseResources()
-    }
-
-    //==============================================================================
-    void paint (Graphics& g) override
-    {
-        // (Our component is opaque, so we must completely fill the background with a solid colour)
-        g.fillAll (Colours::black);
-
-
-        // You can add your drawing code here!
-    }
-
-    void resized() override
-    {
-        // This is called when the MainContentComponent is resized.
-        // If you add any child components, this is where you should
-        // update their positions.
-    }
-
+	void resized() override
+	{
+		
+	}
 
 private:
-    //==============================================================================
+	static String getMidiMessageDescription(const MidiMessage& m)
+	{
+		if (m.isNoteOn())           return "Note on " + MidiMessage::getMidiNoteName(m.getNoteNumber(), true, true, 3);
+		if (m.isNoteOff())          return "Note off " + MidiMessage::getMidiNoteName(m.getNoteNumber(), true, true, 3);
+		if (m.isProgramChange())    return "Program change " + String(m.getProgramChangeNumber());
+		if (m.isPitchWheel())       return "Pitch wheel " + String(m.getPitchWheelValue());
+		if (m.isAftertouch())       return "After touch " + MidiMessage::getMidiNoteName(m.getNoteNumber(), true, true, 3) + ": " + String(m.getAfterTouchValue());
+		if (m.isChannelPressure())  return "Channel pressure " + String(m.getChannelPressureValue());
+		if (m.isAllNotesOff())      return "All notes off";
+		if (m.isAllSoundOff())      return "All sound off";
+		if (m.isMetaEvent())        return "Meta event";
 
-    // Your private member variables go here...
+		if (m.isController())
+		{
+			String name(MidiMessage::getControllerName(m.getControllerNumber()));
 
+			if (name.isEmpty())
+				name = "[" + String(m.getControllerNumber()) + "]";
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+			return "Controller " + name + ": " + String(m.getControllerValue());
+		}
+
+		return String::toHexString(m.getRawData(), m.getRawDataSize());
+	}
+
+	/** Starts listening to a MIDI input device, enabling it if necessary. */
+	void setMidiInput(int index)
+	{
+		const StringArray list(MidiInput::getDevices());
+
+		deviceManager.removeMidiInputCallback(list[lastInputIndex], this);
+
+		const String newInput(list[index]);
+
+		if (!deviceManager.isMidiInputEnabled(newInput))
+			deviceManager.setMidiInputEnabled(newInput, true);
+
+		deviceManager.addMidiInputCallback(newInput, this);
+		midiInputList.setSelectedId(index + 1, dontSendNotification);
+
+		lastInputIndex = index;
+	}
+
+	void comboBoxChanged(ComboBox* box) override
+	{
+		if (box == &midiInputList)
+			setMidiInput(midiInputList.getSelectedItemIndex());
+	}
+
+	// These methods handle callbacks from the midi device + on-screen keyboard..
+	void handleIncomingMidiMessage(MidiInput* source, const MidiMessage& message) override
+	{
+		const ScopedValueSetter<bool> scopedInputFlag(isAddingFromMidiInput, true);
+	}
+	
+	//==============================================================================
+	AudioDeviceManager deviceManager;           // [1]
+	ComboBox midiInputList;                     // [2]
+	Label midiInputListLabel;
+	int lastInputIndex;                         // [3]
+	bool isAddingFromMidiInput;                 // [4]
+
+	//==============================================================================
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainContentComponent);
 };
 
 
